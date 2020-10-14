44e93c8c51130c6cb1477fcedd97f25676237073
Manish Das <manishdas11811480@gmail.com>
2020-10-14 17:09:48 +0530

    Add all my files

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require ('path');
 const app = express();
-app.use(express.static(___dirname + '/dist/practice'));
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(___dirname+
-        '/dist/practice/index.html'));});
+app.use(express.static('/dist/practice'));
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
+  );
+  });
 app.listen(process.env.PORT || 8080);
